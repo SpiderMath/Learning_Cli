de9ccf68a436a62f98befc26892bf7d17fdec570
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-01 23:37:52 +0530

    Working on arg length, fixed no arg problem

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::env;
 fn main() {
     let arguments: Vec<String> = env::args().collect();
 
+    // arguments will always have the value of "target\\debug\\todo_cli.exe" at index 0
+    if arguments.len() == 1 { return; }
+
+    // If there is some command then send it
     let command = arguments[1].clone();
 
     println!("{}", command);
