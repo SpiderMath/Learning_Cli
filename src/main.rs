3dfbec0c5f5d338047e30b16e983d6e768db895b
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-02 12:48:50 +0530

    Added something? :shrug:

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use std::env::args;
 
+fn help() {
+    println!("Displaying help menu...");
+}
+
 fn main() {
-    let arguments: Vec<String> = args().collect();
+    let mut arguments: Vec<String> = args().collect();
 
     // arguments[0] = 'target/release/cli.exe'
+    if arguments.len() == 1 {
+        return help()
+    }
 
-    println!("{:?}", arguments)
+    // Removing the unnecessary 'target/release/cli.exe', it ain't required
+    arguments.remove(0);
+
+    // Getting the command name
+    let cmd_name: String= arguments[0].clone();
+
+    // Then removing that too
+    arguments.remove(0);
+
+    println!("{}", cmd_name);
 }
